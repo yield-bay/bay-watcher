bf048eda6b938180de8149539cef732f67f901d1
Jack Sparrow <capjacksparrow42@protonmail.com>
2022-07-02 06:46:28 +0530

    fetch prices from mongoDB

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,55 @@ use ethers::{types::Address, utils::keccak256};
 use ethers_providers::Ws;
 use eyre::Result;
 use std::convert::TryFrom;
+use std::fmt::format;
 use std::ops::Mul;
 use std::sync::Arc;
+
+use mongodb::{options::ClientOptions, Client};
+// This trait is required to use `try_next()` on the cursor
+use futures::stream::TryStreamExt;
+use mongodb::{
+    bson::{doc, Document},
+    options::FindOptions,
+};
+
 mod contracts;
 use dotenv::dotenv;
 #[path = "./utils/addresses.rs"]
 mod addresses;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Pool {
+    address: String,
+    token0address: String,
+    token1address: String,
+    chainId: i32,
+    feesAPR: f64,
+    official: bool,
+    community: bool,
+    price: f64,
+    reserveUSD: f64,
+    oneDayVolumeUSD: f64,
+    totalSupply: f64,
+    token0Logo: String,
+    token1Logo: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Token {
+    name: String,
+    symbol: String,
+    address: String,
+    decimals: i32,
+    chainId: i32,
+    official: bool,
+    community: bool,
+    price: f64,
+    liquidity: f64,
+    logo: String,
+}
 
 // abigen!(
 //     IUniswapV2Pair,
@@ -36,6 +79,48 @@ abigen!(
 async fn main() -> Result<()> {
     println!("\nStart!\n");
     dotenv().ok();
+
+    // Parse a connection string into an options struct.
+    let mongo_uri = dotenv::var("DB_CONN_STRING").unwrap();
+    println!("mongo_uri: {}", mongo_uri);
+
+    // Parse a connection string into an options struct.
+    let mut client_options = ClientOptions::parse(mongo_uri).await?;
+
+    // Manually set an option.
+    client_options.app_name = Some("My App".to_string());
+
+    // Get a handle to the deployment.
+    let client = Client::with_options(client_options)?;
+
+    // List the names of the databases in that deployment.
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+
+    // Get a handle to a database.
+    let db = client.database("myFirstDatabase");
+
+    // List the names of the collections in that database.
+    for collection_name in db.list_collection_names(None).await? {
+        println!("{}", collection_name);
+    }
+
+    // Get a handle to a collection in the database.
+    let poolsCollection = db.collection::<Pool>("pools");
+    let tokensCollection = db.collection::<Token>("tokens");
+
+    // let filter = doc! { "address": "0xDfEeFA89639125D22ca86E28ce87B164f41AFaE6" };
+    // let filter = doc! { "token0symbol": "WMOVR" };
+    // let find_options = FindOptions::builder().sort(doc! { "price": 1.0 }).build();
+
+    // let mut cursor = poolsCollection.find(filter, None).await?;
+    // while let Some(pool) = cursor.try_next().await? {
+    //     println!("p: {}", pool);
+    // }
+
+    // let pool = poolsCollection.find_one(filter, None).await?;
+    // println!("pool: {:?}", pool.unwrap());
 
     let pk = dotenv::var("PRIVATE_KEY").unwrap();
     let wallet: LocalWallet = pk.parse().expect("fail parse");
@@ -94,7 +179,7 @@ async fn main() -> Result<()> {
             deposit_fee_bp,
             harvest_interval,
             total_lp,
-        ) = chef
+        ): (Address, _, _, _, _, _, _) = chef
             .pool_info(ethers::prelude::U256::from(pid))
             .call()
             .await?;
@@ -108,6 +193,9 @@ async fn main() -> Result<()> {
             harvest_interval,
             total_lp
         );
+
+        // let lp_addr: String = String::from(lp_token);
+        // ethers::prelude::H
 
         let rewarders = chef
             .pool_rewarders(ethers::prelude::U256::from(pid))
@@ -126,38 +214,92 @@ async fn main() -> Result<()> {
 
         // TODO: for multi reward farms, calc sum of aprs of all the reward tokens
 
-        if rewards_per_sec.len() > 0 && symbols[0] == "SOLAR" {
-            // TODO: fetch prices from db, fix overflows/typecasting
-            println!("thiss");
-            let solar_price = 1;
-            let spl: U256 = ethers::prelude::U256::from(1);
+        if rewards_per_sec.len() > 0 {
+            let mut total_farm_apr = 0.0;
+            for i in 0..symbols.len() {
+                println!("rwrd[{}]", i);
 
-            let lp_price = 1;
-            let lpp: U256 = ethers::prelude::U256::from(1);
+                let s = format!("{:?}", symbols[i].clone());
+                println!("s: {}", s);
+                let tcsa = ethers::utils::to_checksum(&addresses[i].to_owned(), None); //1285
+                println!("tcsa: {:?}", tcsa);
+                // let token_filter = doc! { "symbol": s };
+                let token_filter = doc! { "address": tcsa };
+                let token = tokensCollection.find_one(token_filter, None).await?;
+                let token_price = token.unwrap().price;
+                println!("token: {:?}", token_price);
 
-            let sepd: u128 = rewards_per_sec[0].as_u128() * 60 * 60 * 24;
-            let ptvl: u128 = total_lp.as_u128();
+                // let a = addresses[i].to_string();
+                // println!("a: {} | {}", addresses[i], a);
 
-            let sepdl: U256 = rewards_per_sec[0];
-            let ptvll: U256 = total_lp;
-            let v = ((sepdl.full_mul(spl)).checked_div(ptvll.full_mul(spl))).unwrap();
-            // .mul(365);
+                // let a = lp_token.to_owned();
+                // println!(
+                //     "a: {} | {:?} | {:?} | {}",
+                //     a,
+                //     a.to_string(),
+                //     lp_token.to_owned(),
+                //     a.to_string() == "0x069C2065100b4D3D982383f7Ef3EcD1b95C05894"
+                // );
+                let csa = ethers::utils::to_checksum(&lp_token.to_owned(), None); //1285
+                println!("csa: {:?}", csa);
 
-            println!("v {}", v);
+                let ms = format!("{:?}", lp_token.to_owned());
+                println!(
+                    "ms: {} | {}",
+                    ms,
+                    ms == "0x069c2065100b4d3d982383f7ef3ecd1b95c05894"
+                );
+                let pool_filter = doc! { "address": csa }; // "0xDfEeFA89639125D22ca86E28ce87B164f41AFaE6" };
+                let pool = poolsCollection.find_one(pool_filter, None).await?;
+                // println!("pool: {:?}", pool.unwrap());
+                if pool.is_some() {
+                    let pool_price = pool.unwrap().price;
+                    println!("pool: {:?}", pool_price);
 
-            // poolAPR
-            println!(
-                "{} {} {} {}",
-                rewards_per_sec[0].as_u128(),
-                total_lp.as_u128(),
-                sepd,
-                ptvl
-            );
-            let farm_apr = ((sepd * 1 / 10) / (ptvl * 134493)) * 365;
-            println!("farmAPR: {}", farm_apr);
+                    // let mut cursor = poolsCollection.find(poolFilter, None).await?;
+                    // while let Some(pool) = cursor.try_next().await? {
+                    //     println!("price: {:?}", pool.price);
+                    // }
 
-            // feeAPR
-            // let trading_apr = (lastDayVolume * 0.002 * 365 * 100) / pairLiquidity;
+                    // TODO: fetch prices from db, fix overflows/typecasting
+                    println!("thiss");
+                    let solar_price = 1;
+                    let spl: U256 = ethers::prelude::U256::from(1);
+
+                    let lp_price = 1;
+                    let lpp: U256 = ethers::prelude::U256::from(1);
+
+                    let sepd: u128 = rewards_per_sec[0].as_u128() * 60 * 60 * 24;
+                    let ptvl: u128 = total_lp.as_u128();
+
+                    let sepdl: U256 = rewards_per_sec[0];
+                    let ptvll: U256 = total_lp;
+                    let v = ((sepdl.full_mul(spl)).checked_div(ptvll.full_mul(spl))).unwrap();
+                    // .mul(365);
+
+                    println!("v {}", v);
+
+                    // poolAPR
+                    println!(
+                        "{} {} {} {}",
+                        rewards_per_sec[0].as_u128(),
+                        total_lp.as_u128(),
+                        sepd,
+                        ptvl
+                    );
+                    let farm_apr =
+                        ((sepd as f64 * token_price) / (ptvl as f64 * pool_price)) * 365.0;
+                    println!("farmAPR: {}", farm_apr);
+                    total_farm_apr += farm_apr;
+
+                    // feeAPR
+                    // let trading_apr = (lastDayVolume * 0.002 * 365 * 100) / pairLiquidity;
+                } else {
+                    // TODO: doesn't work for stable amm pools, veSolar
+                    println!("can't find pool");
+                }
+            }
+            println!("total_farm_apr: {:?}", total_farm_apr);
         }
     }
 
