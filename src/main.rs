2985023e8e8ed1a20c0da00a4918cb8ae1778cc5
Jack Sparrow <capjacksparrow42@protonmail.com>
2023-07-03 16:30:02 +0530

    pause demeter

--- src/main.rs
+++ src/main.rs
@@ -50,10 +50,10 @@ async fn run_jobs() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .unwrap();
 
-    println!("------------------------------\ndemeter_jobs");
-    custom::demeter::demeter_jobs(mongo_uri.clone())
-        .await
-        .unwrap();
+    // println!("------------------------------\ndemeter_jobs");
+    // custom::demeter::demeter_jobs(mongo_uri.clone())
+    //     .await
+    //     .unwrap();
 
     println!("------------------------------\ncurve_jobs");
     custom::curve::curve_jobs(mongo_uri.clone()).await.unwrap();
